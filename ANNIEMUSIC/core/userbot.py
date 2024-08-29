c542328a4a91f2b22c9ae686cf3b4c4c56edec6b
Kittyxmusic <nnmoon692@gmail.com>
2024-08-29 23:27:19 +0530

    Update userbot.py

--- ANNIEMUSIC/core/userbot.py
+++ ANNIEMUSIC/core/userbot.py
@@ -51,9 +51,9 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("Dora_hub")
+                await self.one.join_chat("Kittyxupdates")
               
-                await self.one.join_chat("loggchk")
+                await self.one.join_chat("Grandxmasti")
             except:
                 pass
             assistants.append(1)
@@ -74,9 +74,9 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("Dora_hub")
+                await self.two.join_chat("Kittyxupdates")
                 
-                await self.one.join_chat("loggchk")
+                await self.one.join_chat("Grandxmasti")
             except:
                 pass
             assistants.append(2)
@@ -96,9 +96,9 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("Dora_hub")
+                await self.three.join_chat("Kittyxupdates")
                 
-                await self.one.join_chat("loggchk")
+                await self.one.join_chat("Grandxmasti")
             except:
                 pass
             assistants.append(3)
@@ -118,9 +118,9 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("Dora_hub")
+                await self.four.join_chat("Kittyxupdates")
                 
-                await self.one.join_chat("loggchk")
+                await self.one.join_chat("Grandxmasti")
             except:
                 pass
             assistants.append(4)
@@ -140,9 +140,9 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("Dora_hub")
+                await self.five.join_chat("Kittyxupdates")
                 
-                await self.one.join_chat("loggchk")
+                await self.one.join_chat("Grandxmasti")
             except:
                 pass
             assistants.append(5)

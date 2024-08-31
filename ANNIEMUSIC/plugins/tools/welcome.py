2b06567567ada05fa7a8abe09e635cf76d8669e2
Kittyxmusic <nnmoon692@gmail.com>
2024-08-31 17:50:27 +0530

    Update welcome.py

--- ANNIEMUSIC/plugins/tools/welcome.py
+++ ANNIEMUSIC/plugins/tools/welcome.py
@@ -96,8 +96,8 @@ def welcomepic(pic, user, chatname, id, uname, brightness_factor=1.3):
     pfp = circle(pfp, brightness_factor=brightness_factor)
     pfp = pfp.resize((892, 880))
     draw = ImageDraw.Draw(background)
-    font = ImageFont.truetype("ANNIEMUSIC/assets/annie/font.ttf", size=95)
-    welcome_font = ImageFont.truetype("ANNIEMUSIC/assets/annie/font.ttf", size=45)
+    font = ImageFont.truetype("ANNIEMUSIC/assets/annie/font9.ttf", size=95)
+    welcome_font = ImageFont.truetype("ANNIEMUSIC/assets/annie/font9.ttf", size=45)
 
     # Draw user's name with shining red fill and dark saffron border
     draw.text((1770, 1015), f": {user}", fill=(255, 0, 0), font=font)

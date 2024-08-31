3775cd58ad0eef7f7310f5481e1df140d4bd6edb
Kittyxmusic <nnmoon692@gmail.com>
2024-08-31 18:16:35 +0530

    Update thumbnails.py

--- ANNIEMUSIC/utils/thumbnails.py
+++ ANNIEMUSIC/utils/thumbnails.py
@@ -8,7 +8,7 @@ from PIL import Image, ImageDraw, ImageEnhance, ImageFilter, ImageFont
 from unidecode import unidecode
 from youtubesearchpython.__future__ import VideosSearch
 
-from AnonXMusic import app
+from ANNIEMUSIC import app
 from config import YOUTUBE_IMG_URL
 
 
